7761122053a23398ceb0a2dfe767f4e6754ed9d8
Dogukan Demirel <44119023+dogukandmrl@users.noreply.github.com>
2022-07-03 21:16:49 +0300

    Update subcurrency.sol

--- subcurrency.sol
+++ subcurrency.sol
@@ -10,7 +10,7 @@ contract Coin{
     address public minter;
     mapping (address=> uint) public balances;
 
-    event Sent (address form, address to, uint amount);
+    event Sent (address from, address to, uint amount);
 
     constructor(){
         minter = msg.sender;
